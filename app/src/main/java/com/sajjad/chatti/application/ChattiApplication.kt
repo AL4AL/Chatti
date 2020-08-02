a9d8eb4a4557416b027c89a1c664c5f26f2f62bf
professor_sajjad <kalantar98@gmail.com>
2020-08-03 00:22:25 +0430

    Register User and Message models

--- app/src/main/java/com/sajjad/chatti/application/ChattiApplication.kt
+++ app/src/main/java/com/sajjad/chatti/application/ChattiApplication.kt
@@ -1,13 +1,17 @@
 package com.sajjad.chatti.application
 
 import android.app.Application
+import com.example.base.domain.model.User
 import com.parse.Parse
+import com.parse.ParseObject
+import com.parse.ParseUser
 import com.sajjad.application_component.ApplicationComponent
 import com.sajjad.application_component.ApplicationComponentProvider
 import com.sajjad.application_component.DaggerApplicationComponent
 import com.sajjad.application_component.context.component.DaggerContextComponent
 import com.sajjad.application_component.network.component.DaggerNetworkComponent
 import com.sajjad.application_component.parse.component.DaggerParseComponent
+import com.sajjad.base.domain.model.Message
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -37,6 +41,7 @@ class ChattiApplication : Application(), ApplicationComponentProvider {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         DaggerChattiApplicationComponent.factory()
             .create(
                 applicationComponent = this.applicationComponent,
@@ -48,6 +53,8 @@ class ChattiApplication : Application(), ApplicationComponentProvider {
     @Inject
     fun initParse() {
         Parse.initialize(this.parseConfiguration)
+        ParseUser.registerSubclass(User::class.java)
+        ParseObject.registerSubclass(Message::class.java)
         Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE)
         Timber.v("Parse initialized")
     }
