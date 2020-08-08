79945b90f1b4ed7cba60d9a789a9eca28eace927
professor_sajjad <kalantar98@gmail.com>
2020-08-08 22:43:50 +0430

    Add navigation drawer

--- app/src/main/java/com/sajjad/chatti/main/MainActivity.kt
+++ app/src/main/java/com/sajjad/chatti/main/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.sajjad.chatti.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import com.sajjad.chatti.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.navController = findNavController(R.id.navHostFragment)
+        setUpNavigationWithAppbar()
+        setUpNavigationWithDrawer()
+    }
+
+    private fun setUpNavigationWithAppbar() {
+        setSupportActionBar(my_toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.run {
+                if (destination.id == R.id.loginFragment || destination.id == R.id.signUpFormFragment) {
+                    hide()
+                } else {
+                    show()
+                }
+            }
+        }
+    }
+
+    private fun setUpNavigationWithDrawer() {
+        NavigationUI.setupWithNavController(navigationView, navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment || destination.id == R.id.signUpFormFragment) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                return if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
